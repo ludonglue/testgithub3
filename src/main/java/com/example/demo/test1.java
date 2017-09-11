4717c025011c5bd406a22394364507142d1f3ee9
River <3318591690@qq.com>
2017-09-11 19:33:52 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/test1.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+/**
+ * Created by River on 2017/9/11.
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
